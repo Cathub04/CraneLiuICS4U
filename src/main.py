4db3d79763cfd0e3b688867b56ad524f06f55709
3CNan <wunanxi257@gmail.com>
2022-11-29 15:07:44 -0500

    .

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import pygame
+import sys
+
+BLACK = pygame.Color(0, 0, 0)
+WHITE = pygame.Color(255, 255, 255)
+RED = pygame.Color(255, 0, 0)
+GREY = pygame.Color(150, 150, 150)
+
+pygame.init()
+
+screen = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("Game")
+
+screen.fill(BLACK)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+
+# END
