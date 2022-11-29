b3c06422011f9d76989247a931ee39424223ea46
3CNan <wunanxi257@gmail.com>
2022-11-29 14:23:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import sys
+
 BLACK = pygame.Color(0, 0, 0)
 WHITE = pygame.Color(255, 255, 255)
 RED = pygame.Color(255, 0, 0)
@@ -6,7 +8,18 @@ GREY = pygame.Color(150, 150, 150)
 
 pygame.init()
 
+screen = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("Game")
 
+screen.fill(BLACK)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
 ########
 ########
 # END
