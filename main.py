0d9a14f5ec502e104823d76686e61cabee85df7e
3CNan <wunanxi257@gmail.com>
2022-11-29 15:15:55 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import pygame
+import sys
+import threading
+
+BLACK = pygame.Color(0, 0, 0)
+WHITE = pygame.Color(255, 255, 255)
+RED = pygame.Color(255, 0, 0)
+GREY = pygame.Color(150, 150, 150)
+
+pygame.init()
+
+screen = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("Game")
+
+screen.fill(BLACK)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+
+# END
