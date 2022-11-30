d50288e44ccd3d89aed120e9ab502dfbd9ef75e9
3CNan <wunanxi257@gmail.com>
2022-11-29 20:28:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,14 @@ while True:
             time_status = False
             pygame.quit()
             sys.exit()
-
+        '''
+        background = [terrain1, terrain1, terrain2, terrain2, terrain2, terrain1]
+        screen = create_graphics_screen()
+        for i in range(6):
+            screen.blit(background[i], (i*10, 0))
+        playerpos = 3
+        screen.blit(playerimage, (playerpos*10, 0))
+        '''
     pygame.display.update()
 
 # END
