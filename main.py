7855523c16b2473b3f40c132379ab0677437ea46
3CNan <wunanxi257@gmail.com>
2022-12-01 19:53:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -132,7 +132,6 @@ def jump():
 
 
 def is_collide(p1, p2, p1pos, p2pos):
-    global e_change
     # (surface1, surface2, [x, y], [x, y])
     #   p1.left  > p2.right                       p1.right < p2.left
     if (p1pos[0] + 25 > p2pos[0] + p2.get_width() - 50) or (p1pos[0] + p1.get_width() < p2pos[0] + 25) \
